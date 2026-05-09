583311a29cdb82a282605aca39401550268e36c1
mcarlamartinezm <mcarla@gmail.com>
2026-05-09 19:33:34 -0400

    Proyecto final Backend II

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -18,12 +18,14 @@ router.get('/register', (req, res) => {
 //====== Auth -no activado-
  /*router.get('/profile', auth, (req, res) => { res.render('profile', { user: req.user }); 
  });*/
+ /*Esta versión protege la ruta /profile utilizando solo el middleware `auth`. El middleware valida la existencia y validez del JWT almacenado en la cookie, decodifica su contenido y lo asigna a `req.user`. Si el token es válido, se renderiza la vista profile con la información del usuario. Esta implementación demuestra autenticación basada exclusivamente en JWT, sin aplicar validación adicional por roles.*/
 
 
 //===== AuthSession  -no activado-
 /*router.get('/profile', authSession, (req, res) => {
     res.render('profile', { user: req.session.user });
 });*/
+/*Esta versión utiliza el middleware `authSession`, el cual verifica la existencia de req.session.user  almacenado en el servidor mediante express-session. Si la sesión existe, se renderiza la vista profile utilizando los datos guardados en la sesión. Esta alternativa demuestra un mecanismo de autenticación stateful, donde el estado del usuario se mantiene en el servidor. Se conserva como evidencia del uso de sesiones como alternativa a JWT. */
 
 
 //====== Auth + roles, Ruta protegida con JWT y control de acceso por roles (RBAC)
--- src/strategies/local.strategy.js
+++ src/strategies/local.strategy.js
@@ -1,22 +1,24 @@
-import passport from 'passport';
-import { Strategy as LocalStrategy } from 'passport-local';
-import { UserModel } from '../models/User.model.js';
-import { isValidPassword } from '../utils/hash.js';
+import passport from 'passport'; //instancia principal de Passport
+import { Strategy as LocalStrategy } from 'passport-local'; //estrategia de autenticación local
+import { UserModel } from '../models/User.model.js'; //modelo de usuarios
+import { isValidPassword } from '../utils/hash.js'; //función auxiliar que compara la contraseña ingresada con el hash almacenado en la base de datos. 
 
-//inicializa la estrategia local de passport (autenticacióncon email y contraseña)
+//Se define una función encargada de registrar la estrategia local en Passport. Esta función se ejecuta en app.js, antes de inicializar Passport. 
 export const initializeLocalStrategy = () => {
   passport.use(
-    'local', //nombre de la esrtategia
+    'local', //nombre de la estrategia nombre de la estrategia utilizado posteriormente en passport.authenticate('local') 
     new LocalStrategy(
       {
-        usernameField: 'email', //se define email como identificador en lugar de username
-        passwordField: 'password'
-      },
-      async (email, password, done) => {
-        try { //Búsqueda del usuario en la base de datos
-          const user = await UserModel.findOne({ email });
+        usernameField: 'email',  //se define email como identificador en lugar de username
+        passwordField: 'password'  //se define que password contenga la contraseña enviada por el cliente. 
 
-          if (!user) { //Usuario no encontrado
+      },
+      async (email, password, done) => { //Función de verificación que Passport ejecuta al recibir las credenciales 
+
+        try { //Búsqueda del usuario en la base de datos
+          const user = await UserModel.findOne({ email });  //Búsqueda del usuario en la base de datos (mongo DB)
+
+          if (!user) { //Si el usuario no existe, la autenticación falla. 
             return done(null, false, { message: 'Usuario no encontrado' });
           }
 
@@ -26,7 +28,7 @@ export const initializeLocalStrategy = () => {
 
           return done(null, user); //Usuario autenticado correctamente
         } catch (error) {
-          return done(error); //manejo de errores
+          return done(error); //manejo de errores, en caso de error interno, Passport lo recibe y lo maneja. 
         }
       }
     )
